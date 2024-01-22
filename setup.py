8ff65f06d2c8282379f804ad05f534d46de61292
dhruv-anand-aintech <dhruv.anand@ainorthstartech.com>
2024-01-22 14:54:51 +0530

    move code to src folder

--- setup.py
+++ setup.py
@@ -3,16 +3,13 @@ from setuptools import find_packages, setup
 setup(
     name="vdf_io",
     version=open("VERSION.txt").read().strip(),
-    description="A brief description of your package",
+    description="This library uses a universal format for vector datasets to easily export and import data from all vector databases.",
     long_description=open("README.rst").read(),
     license="Apache 2.0",
     author="Dhruv Anand",
     author_email="dhruv.anand@ainorthstartech.com",
-    packages=[
-        package
-        for package in find_packages(exclude="archive*")
-        if package.startswith("vdf_io")
-    ],
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
     entry_points={
         "console_scripts": [
             "export_vdf = vdf_io.export_vdf:main",
